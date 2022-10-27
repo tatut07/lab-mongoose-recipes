991251c60e2057e55ad8380476c36115828418ca
Tatiana Toledo <tatiana@tatianas-mbp.speedport.ip>
2022-10-27 16:44:18 +0200

    iteration1

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI)
-  .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+mongoose;
+async function connectToDataBase() {
+  try {
+    await mongoose.connect(MONGODB_URI);
+    const recipe1 = {
+      title: "Mac Donalds Fries",
+      level: "Easy Peasy",
+      ingredients: ["Potatoes", "Oil", "Salt"],
+      cuisine: "Classical French",
+      dishType: "other",
+      image: "https://images.media-allrecipes.com/images/75131.jpg",
+      duration: 20,
+      creator: "Chef Ronald",
+    };
+    const newRecipe = Recipe.create(recipe1);
+    console.log(recipe1.title);
+    console.log("Connected to the database");
+  } catch (error) {
+    console.error("Error connecting to the database", error);
+  }
+}
+connectToDataBase();
